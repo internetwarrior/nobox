bbd35c68dbbd579b2e7cedfe086e4a971cbf6816
Akbermet198 <122314250+Akbermet198@users.noreply.github.com>
2023-10-03 11:33:39 +0600

    add lang page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./global.css";
 import App from "./App";
@@ -6,19 +5,12 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import "./i18n.js";
-import { I18nextProvider } from "react-i18next";
-import i18next from "i18next";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <I18nextProvider i18n={i18next}>
     <Provider store={store}>
       <BrowserRouter>
-      {/* <Suspense fallback='...loading'> */}
       <App />
-
-      {/* </Suspense> */}
       </BrowserRouter>
     </Provider>
-  // </I18nextProvider>
 );
